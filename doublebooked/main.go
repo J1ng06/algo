803e486a2bcb2153fed8ee49f1bada9a243ac5a8
Jingnan Chen <jingnanchen@bellmedia.ca>
2018-08-31 03:44:13 -0400

    done

--- doublebooked/main.go
+++ doublebooked/main.go
@@ -9,24 +9,6 @@ import (
 )
 
 func main() {
-	// schedules := []schedule.Schedule{
-	// 	schedule.Schedule{20, 21},
-	// 	schedule.Schedule{9, 10},
-	// 	schedule.Schedule{21, 25},
-	// 	schedule.Schedule{8, 9},
-	// 	schedule.Schedule{12, 13},
-	// 	schedule.Schedule{14, 15},
-	// 	schedule.Schedule{1, 2},
-	// }
-
-	// schedules := []schedule.Schedule{
-	// 	schedule.Schedule{20, 24},
-	// 	schedule.Schedule{30, 39},
-	// 	schedule.Schedule{10, 15},
-	// 	schedule.Schedule{36, 37},
-	// 	schedule.Schedule{24, 28},
-	// 	schedule.Schedule{20, 25},
-	// }
 
 	// schedules := []schedule.Schedule{
 	// 	schedule.Schedule{20, 24},
@@ -77,7 +59,6 @@ func DoubleBooked(schedules []schedule.Schedule) (out [][2]schedule.Schedule) {
 	union := make(map[[2]schedule.Schedule]interface{})
 	for i := 0; i < len(schedules); i++ {
 		err, dup := tree.Insert(schedules[i])
-		fmt.Println(dup)
 		if err != nil {
 			log.Fatal("Failed to build tree ", err.Error())
 		}
--- doublebooked/main_test.go
+++ doublebooked/main_test.go
@@ -24,18 +24,73 @@ func TestDoubleBooked(t *testing.T) {
 				{schedule.Schedule{20, 24}, schedule.Schedule{21, 24}},
 			},
 		},
+		{
+			schedules: []schedule.Schedule{
+				schedule.Schedule{20, 21},
+				schedule.Schedule{9, 10},
+				schedule.Schedule{21, 25},
+				schedule.Schedule{8, 9},
+				schedule.Schedule{12, 13},
+				schedule.Schedule{14, 15},
+				schedule.Schedule{1, 2},
+			},
+			expected: [][2]schedule.Schedule{},
+		},
+		{
+			schedules: []schedule.Schedule{
+				schedule.Schedule{20, 24},
+				schedule.Schedule{30, 39},
+				schedule.Schedule{10, 15},
+				schedule.Schedule{36, 37},
+				schedule.Schedule{24, 28},
+				schedule.Schedule{20, 25},
+			},
+			expected: [][2]schedule.Schedule{
+				{schedule.Schedule{20, 24}, schedule.Schedule{20, 25}},
+				{schedule.Schedule{20, 25}, schedule.Schedule{24, 28}},
+				{schedule.Schedule{30, 39}, schedule.Schedule{36, 37}},
+			},
+		},
+		{
+			schedules: []schedule.Schedule{
+				schedule.Schedule{20, 24},
+				schedule.Schedule{30, 35},
+				schedule.Schedule{10, 15},
+				schedule.Schedule{36, 1000},
+				schedule.Schedule{20, 28},
+				schedule.Schedule{20, 25},
+				schedule.Schedule{8, 9},
+				schedule.Schedule{13, 14},
+				schedule.Schedule{14, 15},
+				schedule.Schedule{5, 999},
+				schedule.Schedule{5, 999},
+				schedule.Schedule{1, 2},
+			},
+			expected: [][2]schedule.Schedule{
+				{schedule.Schedule{5, 999}, schedule.Schedule{5, 999}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{20, 25}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{20, 24}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{30, 35}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{10, 15}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{20, 28}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{36, 1000}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{13, 14}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{14, 15}},
+				{schedule.Schedule{5, 999}, schedule.Schedule{8, 9}},
+
+				{schedule.Schedule{20, 24}, schedule.Schedule{20, 28}},
+				{schedule.Schedule{20, 24}, schedule.Schedule{20, 25}},
+
+				{schedule.Schedule{20, 25}, schedule.Schedule{20, 28}},
+
+				{schedule.Schedule{10, 15}, schedule.Schedule{13, 14}},
+				{schedule.Schedule{10, 15}, schedule.Schedule{14, 15}},
+			},
+		},
 	}
 
 	for i, pair := range testCases {
 		results := DoubleBooked(pair.schedules)
-		if len(pair.expected.([][2]schedule.Schedule)) != len(results) {
-			t.Error(
-				"[ Testcase: TestDoubleBooked ", i, " ]\n",
-				"For Node:     ", fmt.Sprintf("%+v", pair.schedules), "\n",
-				"Expected:", fmt.Sprintf("\n%+v", pair.expected.([][2]schedule.Schedule)), "\n",
-				"Got:     ", fmt.Sprintf("\n%+v", results), "\n",
-			)
-		}
 		resultsMap := make(map[[2]schedule.Schedule]interface{})
 		for _, v := range results {
 			resultsMap[v] = new(interface{})
@@ -43,16 +98,13 @@ func TestDoubleBooked(t *testing.T) {
 		for _, v := range pair.expected.([][2]schedule.Schedule) {
 			delete(resultsMap, v)
 		}
-		if len(resultsMap) > 0 {
-			if len(pair.expected.([][2]schedule.Schedule)) != len(results) {
-				t.Error(
-					"[ Testcase: TestDoubleBooked ", i, " ]\n",
-					"For Node:     ", fmt.Sprintf("%+v", pair.schedules), "\n",
-					"Expected:", fmt.Sprintf("\n%+v", pair.expected.([][2]schedule.Schedule)), "\n",
-					"Got:     ", fmt.Sprintf("\n%+v", results), "\n",
-				)
-			}
-
+		if len(pair.expected.([][2]schedule.Schedule)) != len(results) || len(resultsMap) > 0 {
+			t.Error(
+				"[ Testcase: TestDoubleBooked ", i, " ]\n",
+				"For Node:     ", fmt.Sprintf("%+v", pair.schedules), "\n",
+				"Expected:", fmt.Sprintf("\n%+v", pair.expected.([][2]schedule.Schedule)), "\n",
+				"Got:     ", fmt.Sprintf("\n%+v", results), "\n",
+			)
 		}
 	}
 }
